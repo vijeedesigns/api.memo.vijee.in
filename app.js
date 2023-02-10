2487ab90218727af8e3a7c526e26cc73038ae4d1
Vijeesh P N <40301290+vijeedesigns@users.noreply.github.com>
2023-02-10 18:12:22 +0530

    Updating app.js

--- app.js
+++ app.js
@@ -21,38 +21,3 @@ server.get('/', (req, res) => {
         <div>Welcome to memo.vijee.in API.</div>
     </div>`);
 })
-
-const RouteAuth = require('./src/routes/route-auth');
-server.use('/auth', RouteAuth);
-
-const RouteUsers = require('./src/routes/route-users');
-server.use('/users', RouteUsers);
-
-const RouteExpenses = require('./src/routes/route-expenses');
-server.use('/expenses', RouteExpenses);
-
-const RouteEvents = require('./src/routes/route-events');
-server.use('/events', RouteEvents);
-
-const RouteTransactions = require('./src/routes/route-transactions');
-server.use('/transactions', RouteTransactions);
-
-const RouteDues = require('./src/routes/route-dues');
-server.use('/dues', RouteDues);
-
-const RouteImage = require('./src/routes/route-image');
-server.use('/image', RouteImage);
-
-const RouteCompanies = require('./src/routes/route-companies');
-server.use('/companies', RouteCompanies);
-
-const RouteLocker = require('./src/routes/route-locker');
-server.use('/locker', RouteLocker);
-
-const RouteUploads = require('./src/routes/route-uploads');
-server.use('/uploads', RouteUploads);
-
-const RoutePayslips = require('./src/routes/route-payslips');
-server.use('/payslips', RoutePayslips);
-
-server.listen(process.env.PORT);
