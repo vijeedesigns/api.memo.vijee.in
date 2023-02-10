8fb6bbf6b12e3d6614e2391eb33baac77e70145b
Vijeesh P N <40301290+vijeedesigns@users.noreply.github.com>
2023-02-10 18:17:37 +0530

    Updating app.js

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 const express = require('express');
-const fileupload = require("express-fileupload");
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const dotenv = require('dotenv');
 
 const server = express();
 
-// Set up Global configuration access
-dotenv.config();
-
 server.use(cors());
 server.use(bodyParser.json());
-server.use(fileupload({
-    limits: { fileSize: 50 * 1024 * 1024 },
-}));
 
 server.get('/', (req, res) => {
     res.send(`<div>
