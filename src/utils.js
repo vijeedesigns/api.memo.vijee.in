07058dfe46128eaf672b5153d661722516b5d037
Vijeesh P N <40301290+vijeedesigns@users.noreply.github.com>
2024-03-06 11:21:20 +0530

    Delete src directory

--- src/utils.js
+++
@@ -1,4 +0,0 @@
-const generateGuid = () => Date.now().toString(36) + Math.random().toString(36).substr(2);
-const prePad = (i='', n=2, z='0') => i.toString().padStart(n, z);
-
-module.exports = { generateGuid, prePad };
