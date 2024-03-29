bfe4e3a9c149f7c990c2fa8329b6650a29937b43
Vijeesh P N <40301290+vijeedesigns@users.noreply.github.com>
2024-03-29 11:15:29 +0530

    Add files via upload

--- src/routes/route-image.js
+++ src/routes/route-image.js
@@ -2,21 +2,21 @@ require('../config');
 const express = require('express');
 const RouteImage = express.Router();
 const { response200, response403 } = require('../helpers/responses');
-const Jimp = require("jimp");
 const { imageNotFound } = require('../constants');
 const multer = require('multer');
 const path = require("path");
 const { diskStorage } = require('multer');
+const sharp = require('sharp');
+const mime = require('mime-types');
 
 const readDummyImage = (res) => {
     const buff = Buffer.from(imageNotFound, 'base64');
-    Jimp.read(buff, (err, image) => {
-        if (err) throw err;
-        const mime = image.getMIME();
-        image.getBuffer(mime, (er, buffer) => {
-            if (er) throw er;
-            res.end(buffer);
-        });
+    const image = sharp(buff);
+    const resizedImage = image.resize(100, 100);
+    resizedImage.toBuffer((err, info) => {
+        if (err) console.log(err);
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.end(info);
     });
 }
 
@@ -28,33 +28,27 @@ const storage = diskStorage({
         return callback(null, new Date().getTime() + '-' + file.originalname);
     }
 });
+
 const upload = multer({ storage }).array("files", 12);
 
 // get resized images
 RouteImage.get('/', (req, res) => {
+    const square = Number(req.query?.s) || 0;
     const width = Number(req.query?.w) || 0;
     const height = Number(req.query?.h) || 0;
-    const ratio = Number(width/height);
     const imgPath = `${appRoot}/assets/uploads/${req.query.path}`;
     const acceptableExtensions = ['jpg', 'jpeg', 'png', 'bmp', 'gif'];
     const extension = req.query.path?.split('.').pop();
     if(acceptableExtensions.includes(extension.toLowerCase())) {
-        Jimp.read(imgPath, (err, image) => {
-            if (err) readDummyImage(res);
-            if(image) {
-                const w = image?.bitmap?.width || 50; // the width of the image
-                const h = image?.bitmap?.height || 50; // the height of the image
-                const r = w/h || 1; // the ratio of the image
-                const nw = r>ratio ? width : height*r;
-                const nh = r>ratio ? width/r : height;
-                const jimpImg = width && height ? image?.resize(nw, nh) : image;
-                const mime = jimpImg.getMIME();
-                jimpImg.getBuffer(mime, (er, buffer) => {
-                    if (er) throw er;
-                    res.end(buffer);
-                });
-            } else {
+        const image = sharp(imgPath);
+        const mimeType = mime.lookup(imgPath);
+        const resizedImage = square ? image.resize(square, square) : image.resize(width, height);
+        resizedImage.toBuffer((err, info) => {
+            if (err) {
                 readDummyImage(res);
+            } else {                
+                res.setHeader('Content-Type', mimeType);
+                res.end(info);
             }
         });
     } else {
@@ -69,24 +63,15 @@ RouteImage.get('/square', (req, res) => {
     const acceptableExtensions = ['jpg', 'jpeg', 'png', 'bmp', 'gif'];
     const extension = req.query.path?.split('.').pop();
     if(acceptableExtensions.includes(extension.toLowerCase())) {
-        Jimp.read(imgPath, (err, image) => {
-            if (err) readDummyImage(res);
-            if(image) {
-                const w = image?.bitmap?.width || 50; // the width of the image
-                const h = image?.bitmap?.height || 50; // the height of the image
-                const min = Math.min(w,h);
-                const ratio = min/radius;
-                const toWidth = w/ratio;
-                const toHeight = h/ratio;
-                const newMin = Math.min(toWidth,toHeight);
-                const jimpImg = image?.resize(toWidth, toHeight)?.crop((toWidth/2)-(newMin/2), (toHeight/2)-(newMin/2), newMin, newMin);
-                const mime = jimpImg.getMIME();
-                jimpImg.getBuffer(mime, (er, buffer) => {
-                    if (er) throw er;
-                    res.end(buffer);
-                });
-            } else {
+        const image = sharp(imgPath);
+        const mimeType = mime.lookup(imgPath);
+        const resizedImage = image.resize(radius, radius);
+        resizedImage.toBuffer((err, info) => {
+            if (err) {
                 readDummyImage(res);
+            } else {                
+                res.setHeader('Content-Type', mimeType);
+                res.end(info);
             }
         });
     } else {
@@ -102,4 +87,4 @@ RouteImage.post('/upload', (req, res) => upload(req, res, err => {
     response200(res, 'File list', { data: { success: true, status: 200, message: 'File uploaded', files } });
 }));
 
-module.exports = RouteImage;
+module.exports = { RouteImage, readDummyImage };
