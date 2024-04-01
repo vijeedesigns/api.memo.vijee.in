be8491cbabbc8f2935c08e8d880fea7f5d1ad845
Vijeesh P N <vijeedesigns@gmail.com>
2024-04-01 21:13:34 +0530

    db config changes

--- src/db-connection.js
+++ src/db-connection.js
@@ -1,18 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-});
-
 // var connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'vijeein_vijeesh_narayanan',
-//     password: '4HGBbTo%R&s1',
-//     database: 'vijeein_admin',
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
 // });
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'vijeein_vijeesh_narayanan',
+    password: '4HGBbTo%R&s1',
+    database: 'vijeein_admin',
+});
 
 exports.query = function(sql, callback) {
     connection?.query(sql, function (error, results, fields) {
